8b8ba4bced0092991572d59dba70e0adf10da0b8
Ranjan Kumar <ranjankumar15102@gmail.com>
2024-07-15 01:07:09 +0530

    Register API

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 import { catchAsyncError } from "../middlewares/catchAsyncError.js";
 import ErrarHandler from "../utils/errorHandler.js";
 import {User} from "../models/User.js"
+import {sendToken} from '../utils/sendToken.js'
 
 export const register = catchAsyncError(async (req, res, next) => {
   const { name, email, password } = req.body;
@@ -13,4 +14,19 @@ export const register = catchAsyncError(async (req, res, next) => {
   let user = await User.findOne({});
 
   if(user) return next(new ErrarHandler("User already exist", 409));
+
+//   Upload file on cloudinary
+
+  user = await User.create({
+    name,
+    email,
+    password,
+    avatar:{
+        public_id: "fadsfsda",
+        url: 'asdfasdf',
+    },
+  })
+
+  sendToken(res, user, "Registered Successfully.", 201);
+
 });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,10 @@ import { register } from "../controllers/userController.js";
 
 const router = express.Router();
 
+// To register a new user
 router.route("/register").post(register);
+
+//Login and Logout
+
 
 export default router;

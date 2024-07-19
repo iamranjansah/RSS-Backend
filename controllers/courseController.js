768b52ddf288905b6ce5ac2f387f559e20a8a004
Ranjan Kumar <ranjankumar15102@gmail.com>
2024-07-20 05:20:06 +0530

    Uploading files using Cloudinary, Multer, addLecture, getCourseLectures

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,43 +1,92 @@
-import { catchAsyncError } from "../middlewares/catchAsyncError.js"
-import {Course} from  "../models/Course.js"
-import errorHandler from '../utils/errorHandler.js'
+import { catchAsyncError } from "../middlewares/catchAsyncError.js";
+import { Course } from "../models/Course.js";
+import ErrorHandler from "../utils/errorHandler.js";
+import cloudinary from "cloudinary";
+import getDataUri from "../utils/dataUri.js";
 
-export const getAllCourses = catchAsyncError (
-    async (req, res,next) => {
-        const courses = await Course.find().select("-lectures");
-    
-        res.status(200).json({
-            success: true,
-            courses,
-        }); 
-    
-    }
-);
 
-export const createCourse = catchAsyncError (
-    async (req, res,next) => {
+export const getAllCourses = catchAsyncError(async (req, res, next) => {
+  const courses = await Course.find().select("-lectures");
 
-const {title, description, category, createdBy} = req.body;
+  res.status(200).json({
+    success: true,
+    courses,
+  });
+});
 
-if(!title || !description || !category || !createdBy) return next(new errorHandler("Please add all field", 400))
+export const createCourse = catchAsyncError(async (req, res, next) => {
+  const { title, description, category, createdBy } = req.body;
 
-const file = req.file;
+  if (!title || !description || !category || !createdBy)
+    return next(new ErrorHandler("Please add all field", 400));
 
-await Course.create({
+  const file = req.file;
+
+  const fileUri = getDataUri(file);
+
+  const mycloud = await cloudinary.v2.uploader.upload(fileUri.content);
+
+  await Course.create({
     title,
     description,
     category,
     createdBy,
-    poster:{
-        public_id: 'temp',
-        url: 'temp',
-    }
-})
+    poster:
+    {
+      public_id: mycloud.public_id,
+      url: mycloud.secure_url,
+    },
     
-        res.status(201).json({
-            success: true,
-            message:"Course created successfully, you can create lecture now."
-        }); 
-    
-    }
-);
+  });
+
+  res.status(201).json({
+    success: true,
+    message: "Course created successfully, you can create lecture now.",
+  });
+});
+
+export const getCourseLectures = catchAsyncError(async (req, res, next) => {
+  const course = await Course.findById(req.params.id);
+
+  if (!course) return next(new ErrorHandler("Course not found", 404));
+
+  course.views += 1;
+  await course.save();
+
+  res.status(200).json({
+    success: true,
+    lectures: course.lectures,
+  });
+});
+
+export const addLecture = catchAsyncError(async (req, res, next) => {
+  const { id } = req.params;
+
+  const { title, description } = req.body;
+
+  // const file = req.files;
+
+  const course = await Course.findById(id);
+
+  if (!course) return next(new ErrorHandler("Course not found", 404));
+
+  //Upload file here
+
+  course.lectures.push({
+    title,
+    description,
+    video: {
+      public_id: "temp",
+      url: "temp",
+    },
+  });
+
+  course.noOfVideos = course.lectures.length;
+
+  await course.save();
+
+  res.status(200).json({
+    success: true,
+    message: "Lecture added in course successfully.",
+  });
+});
--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -1,5 +1,6 @@
 import express  from "express"
-import { createCourse, getAllCourses } from "../controllers/courseController.js";
+import { addLecture, createCourse, getAllCourses, getCourseLectures } from "../controllers/courseController.js";
+import singleUpload from "../middlewares/multer.js";
 
 const router = express.Router();
 
@@ -7,8 +8,11 @@ const router = express.Router();
 router.route('/courses').get(getAllCourses);
 
 // create new course - only admin
-router.route('/createcourse').post(createCourse);
+router.route('/createcourse').post(singleUpload,  createCourse);
 
+//Add lecture, delete course, get course details
+
+router.route('/course/:id').get(getCourseLectures).post(singleUpload, addLecture);
 
 
 export default router;

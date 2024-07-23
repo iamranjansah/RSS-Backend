50a686d2eda06554058eb450f8e07d20b5561130
Ranjan Kumar <ranjankumar15102@gmail.com>
2024-07-24 03:24:35 +0530

    Change in course code

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -6,8 +6,21 @@ import getDataUri from "../utils/dataUri.js";
 import {Stats} from "../models/Stats.js"
 
 export const getAllCourses = catchAsyncError(async (req, res, next) => {
-  const courses = await Course.find().select("-lectures");
 
+  const keyword = req.query.keyword || "";
+  const category = req.query.category || "";
+
+  const courses = await Course.find({
+   title:{
+    $regex:keyword,
+    $options: "i"
+   }, 
+    category: {
+    $regex:category,
+    $options: "i"
+   },
+
+  }).select("-lectures");
   res.status(200).json({
     success: true,
     courses,
